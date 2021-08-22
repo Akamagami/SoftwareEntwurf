8a8eafd2d3e26a20edcd91fff7b6f57ee90b409a
Sebastian Adam <sebastian.adam2000@googlemail.com>
2021-08-22 15:03:48 +0200

    Started working on Command enums and processing GUI events

--- src/frontend/controller/EventDetailsController.java
+++ src/frontend/controller/EventDetailsController.java
@@ -1,4 +1,27 @@
 package frontend.controller;
 
+import frontend.EventCommand;
+import frontend.GUIEvent;
+
 public class EventDetailsController extends GUIController {
+
+    public enum Commands implements EventCommand {
+        ;
+
+        private String cmdText;
+        private Class<?> payloadType;
+        private Commands(String cmdText, Class<?> payloadType) {
+            this.cmdText = cmdText;
+            this.payloadType = payloadType;
+        }
+
+        @Override public String getCommandText() { return null; }
+
+        @Override public Class<?> getPayloadType() { return null; }
+    }
+
+    @Override
+    public void processGUIEvent(GUIEvent ge) {
+        super.processGUIEvent(ge);
+    }
 }
