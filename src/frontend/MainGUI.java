b90669be141b9ba25daa8832d7df3a4934053b04
Sebastian Adam <sebastian.adam2000@googlemail.com>
2021-08-28 23:53:53 +0200

    MAINGUICONTROLLLLLER

--- src/frontend/MainGUI.java
+++ src/frontend/MainGUI.java
@@ -31,6 +31,92 @@ public class MainGUI extends JComponent {
     LagerListGUIController lagerListGUIController;
     TeilEventDetailsController teilEventDetailsController;
 
+
+    public MainGUI() {
+
+        speicher = new Speicher();
+        speicher.load();
+
+        frame = new JFrame();
+        frame.setSize(1280, 960);
+        frame.setTitle("EventPlaner");
+        frame.getContentPane().setLayout(new GridLayout(1, 1));
+
+        WindowListener exitListener = new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int confirm = JOptionPane.showOptionDialog(
+                        null, "Wollen Sie die Anwendung schließen?",
+                        "Beenden", JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE, null, null, null);
+                if (confirm == 0) {
+                    speicher.save();
+                    System.exit(0);
+                }
+            }
+        };
+        frame.addWindowListener(exitListener);
+
+        mainGUIController = new MainGUIController(this, this.speicher);
+
+        eventpane = new EventDetailsPane();
+        lagerpane = new LagerDetailsPane();
+        overviewpane = new OverviewPane();
+        teileventspane = new TeilEventDetailsPane();
+
+        EventDetailsÜbersicht eventDetailsÜbersicht = new EventDetailsÜbersicht();
+        EventDetailsTeilevent eventDetailsTeilevent = new EventDetailsTeilevent();
+        EventDetailsKosten eventDetailsKosten = new EventDetailsKosten();
+        eventpane.addTab("Übersicht", eventDetailsÜbersicht);
+        eventpane.addTab("Teilevents", eventDetailsTeilevent);
+        eventpane.addTab("Kosten", eventDetailsKosten);
+
+        HilfsmittelDetails hilfsmittelDetails = new HilfsmittelDetails();
+        lagerpane.addTab("Hilfsmittel", hilfsmittelDetails);
+
+        KalenderGUI kalenderGUI = new KalenderGUI();
+        EventListGUI eventListGUI = new EventListGUI();
+        LagerListGUI lagerListGUI = new LagerListGUI();
+        overviewpane.addTab("Events", eventListGUI);
+        overviewpane.addTab("Lager", lagerListGUI);
+        overviewpane.addTab("Kalender", kalenderGUI);
+
+        TeilEventDetailsMitarbeiter teilEventDetailsMitarbeiter = new TeilEventDetailsMitarbeiter();
+        TeilEventDetailsÜbersicht teilEventDetailsÜbersicht = new TeilEventDetailsÜbersicht();
+        TeilEventDetailsHilfsmittel teilEventDetailsHilfsmittel = new TeilEventDetailsHilfsmittel();
+        teileventspane.addTab("Übersicht", teilEventDetailsÜbersicht);
+        teileventspane.addTab("Mitarbeiter", teilEventDetailsMitarbeiter);
+        teileventspane.addTab("Hilfsmittel", teilEventDetailsHilfsmittel);
+
+        eventDetailsController = new EventDetailsController(eventDetailsÜbersicht, eventDetailsTeilevent, eventDetailsKosten, mainGUIController, this.speicher);
+        eventListGUIController = new EventListGUIController(eventListGUI, mainGUIController, this.speicher);
+        hilfsmittelDetailsController = new HilfsmittelDetailsController(hilfsmittelDetails, this.speicher);
+        kalenderGUIConroller = new KalenderGUIConroller(kalenderGUI);
+        lagerListGUIController = new LagerListGUIController(lagerListGUI, this.speicher);
+        teilEventDetailsController = new TeilEventDetailsController(teilEventDetailsÜbersicht, teilEventDetailsMitarbeiter, teilEventDetailsHilfsmittel, this.speicher, mainGUIController);
+
+        eventDetailsÜbersicht.setController(eventDetailsController);
+        eventDetailsTeilevent.setController(eventDetailsController);
+        eventDetailsKosten.setController(eventDetailsController);
+        eventListGUI.setController(eventListGUIController);
+        hilfsmittelDetails.setController(hilfsmittelDetailsController);
+        kalenderGUI.setController(kalenderGUIConroller);
+        lagerListGUI.setController(lagerListGUIController);
+        teilEventDetailsÜbersicht.setController(teilEventDetailsController);
+        teilEventDetailsMitarbeiter.setController(teilEventDetailsController);
+        teilEventDetailsHilfsmittel.setController(teilEventDetailsController);
+
+
+
+
+
+        frame.add(overviewpane);
+
+        frame.setVisible(true);
+
+    }
+
     public JFrame getFrame() {
         return frame;
     }
@@ -133,90 +219,5 @@ public class MainGUI extends JComponent {
 
     public void setTeilEventDetailsController(TeilEventDetailsController teilEventDetailsController) {
         this.teilEventDetailsController = teilEventDetailsController;
-    }
-
-    public MainGUI() {
-
-        speicher = new Speicher();
-        speicher.load();
-
-        frame = new JFrame();
-        frame.setSize(1280, 960);
-        frame.setTitle("EventPlaner");
-        frame.getContentPane().setLayout(new GridLayout(1, 1));
-
-        WindowListener exitListener = new WindowAdapter() {
-
-            @Override
-            public void windowClosing(WindowEvent e) {
-                int confirm = JOptionPane.showOptionDialog(
-                        null, "Wollen Sie die Anwendung schließen?",
-                        "Beenden", JOptionPane.YES_NO_OPTION,
-                        JOptionPane.QUESTION_MESSAGE, null, null, null);
-                if (confirm == 0) {
-                    speicher.save();
-                    System.exit(0);
-                }
-            }
-        };
-        frame.addWindowListener(exitListener);
-
-        mainGUIController = new MainGUIController(this, this.speicher);
-
-        eventpane = new EventDetailsPane();
-        lagerpane = new LagerDetailsPane();
-        overviewpane = new OverviewPane();
-        teileventspane = new TeilEventDetailsPane();
-
-        EventDetailsÜbersicht eventDetailsÜbersicht = new EventDetailsÜbersicht();
-        EventDetailsTeilevent eventDetailsTeilevent = new EventDetailsTeilevent();
-        EventDetailsKosten eventDetailsKosten = new EventDetailsKosten();
-        eventpane.addTab("Übersicht", eventDetailsÜbersicht);
-        eventpane.addTab("Teilevents", eventDetailsTeilevent);
-        eventpane.addTab("Kosten", eventDetailsKosten);
-
-        HilfsmittelDetails hilfsmittelDetails = new HilfsmittelDetails();
-        lagerpane.addTab("Hilfsmittel", hilfsmittelDetails);
-
-        KalenderGUI kalenderGUI = new KalenderGUI();
-        EventListGUI eventListGUI = new EventListGUI();
-        LagerListGUI lagerListGUI = new LagerListGUI();
-        overviewpane.addTab("Events", eventListGUI);
-        overviewpane.addTab("Lager", lagerListGUI);
-        overviewpane.addTab("Kalender", kalenderGUI);
-
-        TeilEventDetailsMitarbeiter teilEventDetailsMitarbeiter = new TeilEventDetailsMitarbeiter();
-        TeilEventDetailsÜbersicht teilEventDetailsÜbersicht = new TeilEventDetailsÜbersicht();
-        TeilEventDetailsHilfsmittel teilEventDetailsHilfsmittel = new TeilEventDetailsHilfsmittel();
-        teileventspane.addTab("Übersicht", teilEventDetailsÜbersicht);
-        teileventspane.addTab("Mitarbeiter", teilEventDetailsMitarbeiter);
-        teileventspane.addTab("Hilfsmittel", teilEventDetailsHilfsmittel);
-
-        eventDetailsController = new EventDetailsController(eventDetailsÜbersicht, eventDetailsTeilevent, eventDetailsKosten, mainGUIController, this.speicher);
-        eventListGUIController = new EventListGUIController(eventListGUI, mainGUIController, this.speicher);
-        hilfsmittelDetailsController = new HilfsmittelDetailsController(hilfsmittelDetails, this.speicher);
-        kalenderGUIConroller = new KalenderGUIConroller(kalenderGUI);
-        lagerListGUIController = new LagerListGUIController(lagerListGUI, this.speicher);
-        teilEventDetailsController = new TeilEventDetailsController(teilEventDetailsÜbersicht, teilEventDetailsMitarbeiter, teilEventDetailsHilfsmittel, this.speicher, mainGUIController);
-
-        eventDetailsÜbersicht.setController(eventDetailsController);
-        eventDetailsTeilevent.setController(eventDetailsController);
-        eventDetailsKosten.setController(eventDetailsController);
-        eventListGUI.setController(eventListGUIController);
-        hilfsmittelDetails.setController(hilfsmittelDetailsController);
-        kalenderGUI.setController(kalenderGUIConroller);
-        lagerListGUI.setController(lagerListGUIController);
-        teilEventDetailsÜbersicht.setController(teilEventDetailsController);
-        teilEventDetailsMitarbeiter.setController(teilEventDetailsController);
-        teilEventDetailsHilfsmittel.setController(teilEventDetailsController);
-
-
-
-
-
-        frame.add(overviewpane);
-
-        frame.setVisible(true);
-
     }
 }
---
+++ src/frontend/controller/MainGUIController.java
@@ -0,0 +1,109 @@
+package frontend.controller;
+
+import backend.datenbasis.Speicher;
+import de.dhbwka.swe.utils.event.EventCommand;
+import de.dhbwka.swe.utils.event.GUIEvent;
+import frontend.MainGUI;
+import frontend.UIData.EventUI;
+import frontend.UIData.TeilEventUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainGUIController extends GUIController {
+    private MainGUI mainGUI;
+    private Speicher speicher;
+
+    public MainGUI getMainGUI() {
+        return mainGUI;
+    }
+
+    public void setMainGUI(MainGUI mainGUI) {
+        this.mainGUI = mainGUI;
+    }
+
+    public Speicher getSpeicher() {
+        return speicher;
+    }
+
+    public void setSpeicher(Speicher speicher) {
+        this.speicher = speicher;
+    }
+
+    public MainGUIController(MainGUI mainGUI, Speicher speicher) {
+        this.mainGUI = mainGUI;
+        this.speicher = speicher;
+    }
+
+    public enum Commands implements EventCommand {
+        ;
+        private String cmdText;
+        private Class<?> payloadType;
+        private Commands(String cmdText, Class<?> payloadType) {
+            this.cmdText = cmdText;
+            this.payloadType = payloadType;
+        }
+
+
+        @Override
+        public String getCmdText() {
+            return this.cmdText;
+        }
+
+        @Override public Class<?> getPayloadType() {
+            return this.payloadType;
+        }
+    }
+
+    @Override
+    public void processGUIEvent(GUIEvent ge) {
+        if (ge.getCmd().equals(EventListGUIController.Commands.OPEN_EVENTPANE)) {
+            JFrame frame = new JFrame();
+            frame.add(mainGUI.getEventpane());
+            mainGUI.getEventDetailsController().getÜbersicht().getAttributeComponent().clearValues();
+            mainGUI.getEventDetailsController().getÜbersicht().getKontaktBeschreibungComponent().clearValues();
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            frame.pack();
+        }
+        else if (ge.getCmd().equals(EventDetailsController.Commands.CREATE_TEILEVENT)) {
+            JFrame frame = new JFrame();
+            frame.add(mainGUI.getTeileventspane());
+            mainGUI.getTeilEventDetailsController().getTeilEventDetailsÜbersicht().getAttributeComponent().clearValues();
+            mainGUI.getTeilEventDetailsController().getTeilEventDetailsÜbersicht().getKontaktInformation().clearValues();
+            mainGUI.getTeilEventDetailsController().getTeilEventDetailsÜbersicht().getSpecialComponent().clearValues();
+            mainGUI.getTeilEventDetailsController().getTeilEventDetailsÜbersicht().setTeilEventIdent(ge.getData());
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            frame.setSize(new Dimension(800, 600));
+        }
+        else if (ge.getCmd().equals(EventListGUIController.Commands.OPEN_EDIT_EVENTPANE)) {
+            JFrame frame = new JFrame();
+            EventUI event = (EventUI) ge.getData();
+            frame.add(mainGUI.getEventpane());
+            mainGUI.getTeilEventDetailsController().getTeilEventDetailsÜbersicht().getAttributeComponent().clearValues();
+            mainGUI.getTeilEventDetailsController().getTeilEventDetailsÜbersicht().getKontaktInformation().clearValues();
+            mainGUI.getTeilEventDetailsController().getTeilEventDetailsÜbersicht().getSpecialComponent().clearValues();
+            mainGUI.getEventDetailsController().getEvent(event);
+            mainGUI.getTeilEventDetailsController().loadElements();
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            frame.pack();
+        }
+        else if (ge.getCmd().equals(EventDetailsController.Commands.OPEN_EDIT_TEILEVENTPANE)) {
+            JFrame frame = new JFrame();
+            frame.add(mainGUI.getTeileventspane());
+            TeilEventUI teilEventUI = (TeilEventUI) ge.getData();
+            mainGUI.getTeilEventDetailsController().setCurrentTeilEventUI(teilEventUI);
+            mainGUI.getTeilEventDetailsController().loadElements();
+            mainGUI.getTeilEventDetailsController().getTeilEventDetailsÜbersicht().fillAttributes(teilEventUI);
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            frame.pack();
+        }
+        else if (ge.getCmd().equals(EventDetailsController.Commands.RELOAD_PAGE) || ge.getCmd().equals(EventListGUIController.Commands.RELOAD_PAGE)) {
+            mainGUI.getEventListGUIController().loadElements();
+            mainGUI.getTeilEventDetailsController().loadElements();
+        }
+    }
+}
